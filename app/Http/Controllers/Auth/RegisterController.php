d3dbbe91ab1c00e141284165fd8e7ef4217015a9
petrossian <arshak.petrosyan2019@gmail.com>
2022-01-04 15:01:25 +0400

    for server

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,17 +68,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        DB::table('roles')->insert(
-            [
-                'name' => 'user'
-            ],[
-                'name' => 'admin'
-            ],
-        );
-        DB::table('role_user')->insert([
-            'user_id' => 1,
-            'role_id' => 2
-        ]);
+
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,6 +36,17 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
+        DB::table('roles')->insert(
+            [
+                'name' => 'user'
+            ],[
+                'name' => 'admin'
+            ],
+        );
+        DB::table('role_user')->insert([
+            'user_id' => 1,
+            'role_id' => 2
+        ]);
         return view('home');
     }
     public function show($id){
