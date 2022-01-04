1475e2092d192e48fbc487c2fad7387ac4ee411e
petrossian <arshak.petrosyan2019@gmail.com>
2022-01-04 14:47:38 +0400

    for server

--- database/migrations/2021_12_09_112520_create_wishlists_table.php
+++ database/migrations/2021_12_09_112520_create_wishlists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWishlistTable extends Migration
+class CreateWishlistsTable extends Migration
 {
     /**
      * Run the migrations.
