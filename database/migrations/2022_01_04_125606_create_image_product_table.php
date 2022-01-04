5a98b214090f471fd958ace705ccd3d2dc689d75
petrossian <arshak.petrosyan2019@gmail.com>
2022-01-04 16:56:35 +0400

    for server

--- database/migrations/2022_01_04_125606_create_image_product_table.php
+++ database/migrations/2022_01_04_125606_create_image_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductImageTable extends Migration
+class CreateImageProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateProductImageTable extends Migration
      */
     public function up()
     {
-        Schema::create('product_image', function (Blueprint $table) {
+        Schema::create('image_product', function (Blueprint $table) {
             $table->id();
             $table->foreignId('product_id');
             $table->foreignId('image_id');
@@ -28,6 +28,6 @@ class CreateProductImageTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product_image');
+        Schema::dropIfExists('image_product');
     }
 }
